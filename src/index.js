0dca91da7491c1e184b9943995ad06f6f1473666
robberfree <robberfree@qq.com>
2018-02-07 22:39:45 +0800

    1. 将Tween变得更纯粹

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@
   var context = canvas.getContext('2d');
   var flower = document.querySelector('.flower');
 
-  var LEVEL = 9; //整个树结构的层级,[0,LEVEL]
+  var LEVEL = 7; //整个树结构的层级,[0,LEVEL]
   var LENGTH = 200; //树干/树枝的最大长度
   var ANGLE = {
     min: 30,
@@ -46,12 +46,37 @@
 
   function drawTrunkAndBranch(x, y, angle, level, delay) {
     if (level <= LEVEL) {
+      console.log(level);
       var trunkLength = LENGTH * Math.pow(GOLDEN_RATIO, level, delay);
       var radian = angle * ANGLE_2_RADIAN;
       var x2 = x + trunkLength * Math.cos(radian);
       var y2 = y - trunkLength * Math.sin(radian); //canvas的坐标系统与平面直角坐标系y轴相反
       var duration = Math.round(trunkLength * SPEED);
-      new Tween({ x: x, y: y }, { x: x2, y: y2 }, duration, delay);
+      new Tween({ x: x, y: y }, { x: x2, y: y2 }, duration, delay, function(
+        current,
+        last
+      ) {
+        context.beginPath();
+        context.moveTo(last.x, last.y);
+        context.lineTo(current.x, current.y);
+        context.stroke();
+
+        if (Math.random() < 0.02) {
+          var flowerWidth = randomValueInRange(FLOWER_WIDTH);
+          var flowerHeight = flower.width / flower.height * flowerWidth;
+          context.drawImage(
+            flower,
+            0,
+            0,
+            flower.width,
+            flower.height,
+            last.x - flowerWidth * 0.5, //对齐中心点
+            last.y - flowerHeight * 0.5,
+            flowerWidth,
+            flowerHeight
+          );
+        }
+      });
 
       drawTrunkAndBranch(
         x2,
@@ -75,7 +100,7 @@
     Array.prototype.forEach.call(tweens, function(tween) {
       if (tween.delay <= currentFrame && tween.currentFrame <= tween.duration) {
         hasTween = true;
-        tween.draw();
+        tween.on();
       }
     });
     if (liveTween === false) {
@@ -89,48 +114,31 @@
   drawTrunkAndBranch(300, 800, 90, 0, 0);
   animId = requestAnimFrame(update);
 
-  function Tween(from, to, duration, delay) {
+  function Tween(from, to, duration, delay, onUpdate) {
     this.from = this.current = from;
     this.to = to;
     this.duration = duration;
     this.currentFrame = 0;
     this.delay = delay;
+    this.onUpdate = onUpdate;
 
     tweens.push(this);
   }
 
-  Tween.prototype.draw = function() {
-    var x2 =
-      this.from.x +
-      this.currentFrame / this.duration * (this.to.x - this.from.x);
-    var y2 =
-      this.from.y +
-      this.currentFrame / this.duration * (this.to.y - this.from.y);
+  Tween.prototype.on = function() {
+    let current = {};
+    Object.keys(this.from).forEach(
+      function(key) {
+        current[key] =
+          this.from[key] +
+          this.currentFrame / this.duration * (this.to[key] - this.from[key]);
+      }.bind(this)
+    );
+    this.onUpdate(current, this.current);
 
-    context.beginPath();
-    context.moveTo(this.current.x, this.current.y);
-    context.lineTo(x2, y2);
-    context.stroke();
-
-    if (Math.random() < 0.02) {
-      var flowerWidth = randomValueInRange(FLOWER_WIDTH);
-      var flowerHeight = flower.width / flower.height * flowerWidth;
-      context.drawImage(
-        flower,
-        0,
-        0,
-        flower.width,
-        flower.height,
-        this.current.x - flowerWidth * 0.5, //对齐中心点
-        this.current.y - flowerHeight * 0.5,
-        flowerWidth,
-        flowerHeight
-      );
-    }
-
-    this.current = { x: x2, y: y2 };
+    this.current = current;
     this.currentFrame += 1;
   };
 
-  function Flower() {}
+  function Flower(context, x, y) {}
 })();
