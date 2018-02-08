6aca10f49ffbbee0646cd11a2c041c073dc9f8e4
robberfree <robberfree@qq.com>
2018-02-08 19:35:32 +0800

    1. 继续模块化

--- src/index.js
+++ src/index.js
@@ -27,59 +27,105 @@ var Tree = (function() {
     function(id) {
       clearTimeout(id);
     };
-
-  var canvas = document.querySelector('canvas');
-  var flower = document.querySelector('.flower');
-
-  var tweens = [];
-  var currentFrame = 0;
-  var animId;
-  var flowerPositions = [];
-
-  function update() {
-    var liveTween = false;
-    Array.prototype.forEach.call(tweens, function(tween) {
-      if (tween.delay <= currentFrame && tween.currentFrame <= tween.duration) {
-        liveTween = true;
-        tween.on();
-      }
-    });
-    if (liveTween === false) {
-      cancelAnimFrame(animId);
-      tree.bloom();
-    }
-
-    currentFrame += 1;
-    animId = requestAnimFrame(update);
+  //Tween
+  function Tween(from, to, duration, delay, onUpdate) {
+    this.from = this.current = from;
+    this.to = to;
+    this.duration = duration;
+    this.currentFrame = 0;
+    this.delay = delay;
+    this.onUpdate = onUpdate;
   }
 
+  Tween.prototype = {
+    on: function() {
+      let current = {};
+      Object.keys(this.from).forEach(
+        function(key) {
+          current[key] =
+            this.from[key] +
+            this.currentFrame / this.duration * (this.to[key] - this.from[key]);
+        }.bind(this)
+      );
+
+      if (this.onUpdate) this.onUpdate(current, this.current);
+
+      this.current = current;
+      this.currentFrame += 1;
+    }
+  };
+  //Flower
+  function Flower(context, x, y, options) {
+    var flowerWidth = randomValueInRange(options.width);
+    var flowerHeight = options.image.width / options.image.height * flowerWidth;
+    context.globalAlpha = Math.random();
+    context.drawImage(
+      options.image,
+      0,
+      0,
+      options.image.width,
+      options.image.height,
+      x - flowerWidth * 0.5, //对齐中心点
+      y - flowerHeight * 0.5,
+      flowerWidth,
+      flowerHeight
+    );
+    context.globalAlpha = 1;
+  }
+
+  //Tree
   function Tree(canvas, options) {
     this.context = canvas.getContext('2d');
     this.flowers = [];
+    this.tweens = [];
+    this.currentFrame = 0;
+    this.animId = undefined;
 
     options = or(options, {});
     options.flower = or(options.flower, {});
-
     this.options = {
-      startX: or(options.startX, 300),
-      startY: or(options.startY, 800),
+      startX: or(options.startX, canvas.width * 0.5),
+      startY: or(options.startY, canvas.height),
       startAngle: or(options.startAngle, 90),
       strokeStyle: or(options.strokeColor, '#000'),
-      level: or(options.level, 7),
-      length: or(options.length, 200),
-      thicknessPerLevel: or(options.thicknessPerLevel, 1),
+      widthPerLevel: or(options.widthPerLevel, 1),
+      lengthPerLevel: or(options.lengthPerLevel, 30),
+      level: or(options.level, 8),
       angle: or(options.angle, {
         min: 30,
         max: 60
       }),
       framePerPx: or(options.framePerPx, 0.1),
       flower: {
-        width: or(options.flower.width, { min: 4, max: 8 })
+        width: or(options.flower.width, { min: 4, max: 12 }),
+        image: options.flower.image
       }
     };
   }
 
   Tree.prototype = {
+    update: function() {
+      var liveTween = false;
+      this.tweens.forEach(
+        function(tween) {
+          if (
+            tween.delay <= this.currentFrame &&
+            tween.currentFrame <= tween.duration
+          ) {
+            liveTween = true;
+            tween.on();
+          }
+        }.bind(this)
+      );
+
+      if (liveTween === false) {
+        cancelAnimFrame(this.animId);
+        this.bloom();
+      } else {
+        this.currentFrame += 1;
+        animId = requestAnimFrame(this.update.bind(this));
+      }
+    },
     grow: function() {
       this.drawTrunkAndBranch(
         this.options.startX,
@@ -88,30 +134,36 @@ var Tree = (function() {
         1,
         0
       );
+
+      this.animId = requestAnimFrame(this.update.bind(this));
+
       return this;
     },
     drawTrunkAndBranch: function(x, y, angle, level, delay) {
       if (level <= this.options.level) {
-        var trunkLength =
-          this.options.length * Math.pow(GOLDEN_RATIO, level - 1, delay);
+        var length = this.getValue(level, this.options.lengthPerLevel);
         var radian = angle * ANGLE_2_RADIAN;
-        var x2 = x + trunkLength * Math.cos(radian);
-        var y2 = y - trunkLength * Math.sin(radian); //canvas的坐标系统与平面直角坐标系y轴相反
-        var duration = Math.round(trunkLength * this.options.framePerPx);
-        new Tween(
-          { x: x, y: y },
-          { x: x2, y: y2 },
-          duration,
-          delay,
-          function(current, last) {
-            this.context.beginPath();
-            this.context.strokeStyle = this.options.strokeStyle;
-            this.context.lineWidth =
-              (this.options.level - level + 1) * this.options.thicknessPerLevel;
-            this.context.moveTo(last.x, last.y);
-            this.context.lineTo(current.x, current.y);
-            this.context.stroke();
-          }.bind(this)
+        var x2 = x + length * Math.cos(radian);
+        var y2 = y - length * Math.sin(radian); //canvas的坐标系统与平面直角坐标系y轴相反
+        var duration = Math.round(length * this.options.framePerPx);
+        this.tweens.push(
+          new Tween(
+            { x: x, y: y },
+            { x: x2, y: y2 },
+            duration,
+            delay,
+            function(current, last) {
+              this.context.beginPath();
+              this.context.strokeStyle = this.options.strokeStyle;
+              this.context.lineWidth = this.getValue(
+                level,
+                this.options.widthPerLevel
+              );
+              this.context.moveTo(last.x, last.y);
+              this.context.lineTo(current.x, current.y);
+              this.context.stroke();
+            }.bind(this)
+          )
         );
 
         this.drawTrunkAndBranch(
@@ -132,10 +184,11 @@ var Tree = (function() {
         this.getFlowerPosition(x, y, x2, y2);
       }
     },
-    /**
-     * 在(x2,y2)，线段的两个黄金分割点，选取一个点绘制flower
-     */
+    getValue: function(level, per) {
+      return Math.pow(GOLDEN_RATIO, level - 1) * this.options.level * per;
+    },
     getFlowerPosition: function(x1, y1, x2, y2) {
+      //从(x2,y2)、线段的两个黄金分割点中选取一个点绘制flower
       var random = Math.random();
       var position = {};
 
@@ -153,58 +206,36 @@ var Tree = (function() {
       this.flowers.push(position);
     },
     bloom: function() {
+      let img = this.options.flower.image;
       this.flowers.forEach(
         function(_flower) {
-          var flowerWidth = randomValueInRange(this.options.flower.width);
-          var flowerHeight = flower.width / flower.height * flowerWidth;
-          this.context.drawImage(
-            flower,
-            0,
-            0,
-            flower.width,
-            flower.height,
-            _flower.x - flowerWidth * 0.5, //对齐中心点
-            _flower.y - flowerHeight * 0.5,
-            flowerWidth,
-            flowerHeight
-          );
+          new Flower(this.context, _flower.x, _flower.y, this.options.flower);
         }.bind(this)
       );
     }
   };
 
-  function Tween(from, to, duration, delay, onUpdate) {
-    this.from = this.current = from;
-    this.to = to;
-    this.duration = duration;
-    this.currentFrame = 0;
-    this.delay = delay;
-    this.onUpdate = onUpdate;
-
-    tweens.push(this);
-  }
-
-  Tween.prototype.on = function() {
-    let current = {};
-    Object.keys(this.from).forEach(
-      function(key) {
-        current[key] =
-          this.from[key] +
-          this.currentFrame / this.duration * (this.to[key] - this.from[key]);
-      }.bind(this)
-    );
-    this.onUpdate(current, this.current);
-
-    this.current = current;
-    this.currentFrame += 1;
-  };
-
-  var tree = new Tree(canvas).grow();
-  var tree2 = new Tree(canvas, {
-    startX: 0,
-    startY: 800
-  }).grow();
-  animId = requestAnimFrame(update);
-
   return Tree;
+})();
+
+(function() {
+  var canvas = document.querySelector('canvas');
+  var flower = document.querySelector('.flower');
+
+  var tree = new Tree(canvas, {
+    flower: {
+      image: flower
+    }
+  }).grow();
+
+  canvas.addEventListener('click', function(e) {
+    var tree = new Tree(canvas, {
+      startX: e.clientX,
+      startY: canvas.height,
+      level: Math.round(Math.random() * 8),
+      flower: {
+        image: flower
+      }
+    }).grow();
+  });
 })();
